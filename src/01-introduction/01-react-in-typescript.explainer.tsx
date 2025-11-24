8bef7e54fa04ad7bce10e8a53d0349a2a73178f1
lamisalqahtani <l.alqahtani-t@elc.edu.sa>
2025-11-24 12:03:00 +0300

    test

--- src/01-introduction/01-react-in-typescript.explainer.tsx
+++ src/01-introduction/01-react-in-typescript.explainer.tsx
@@ -1,3 +1,8 @@
 // Explain how React works with TypeScript
 // tsconfig options: jsx: "preserve"
 // @types/react
+import React from "react";
+const Component = () => {
+  // error under `div`
+  return <div></div>
+}
